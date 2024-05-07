159124b1cae2a5970ad4816a4ee9d1907fe7804f
Tin Marusic <tmarusic@lxplus802.cern.ch>
2024-05-07 17:23:53 +0200

    english translate

--- Plot/plot_sum.py
+++ Plot/plot_sum.py
@@ -5,120 +5,121 @@ from shapely.geometry import shape
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib.colors import Normalize
 
-#os.chdir("Plot")
-coord = [[[],[]],[[],[]]] #za svaku dimeziju(E i H) - u prvi element spremamo koordinate,u drugi sume
-files = [["out_eta_E.txt","out_phi_E.txt","out_sum_E.txt"],["out_eta_H.txt","out_phi_H.txt","out_sum_H.txt",]]
-imena = ["E","H"]
+# os.chdir("Plot")
+coords = [[[], []], [[], []]]  # for each dimension (E and H) - in the first element we store the coordinates, in the second the sums
+files = [["out_eta_E.txt", "out_phi_E.txt", "out_sum_E.txt"], ["out_eta_H.txt", "out_phi_H.txt", "out_sum_H.txt"]]
+names = ["E", "H"]
 
 for i in range(2):
     j = 0
-    brojac = 0
+    counter = 0
     for file in files[i]:
         with open(file, 'r') as f:
             lines = f.readlines()
             for line in lines:
-                coord[i][j].append(float(line.strip("\n")))
-        brojac += 1
-        if brojac > 1:
-            j += 1 
+                coords[i][j].append(float(line.strip("\n")))
+        counter += 1
+        if counter > 1:
+            j += 1
 
-    #slazemo parove x,y koordinata
-                
-    a = coord[i][0][:len(coord[i][0])//2] #buduci da je prva polovica niza eta koord, a druga phi
-    b = coord[i][0][len(coord[i][0])//2:]
+    # create pairs of x, y coordinates
+    a = coords[i][0][:len(coords[i][0]) // 2]  # since the first half of the array is eta coord, and the second is phi
+    b = coords[i][0][len(coords[i][0]) // 2:]
     c = []
-    for j in range(len(a)): 
+    for j in range(len(a)):
         c.append(a[j])
         c.append(b[j])
-        
-    coord[i][0] = [c[k:k+2] for k in range(0, len(c), 2)] 
+
+    coords[i][0] = [c[k:k + 2] for k in range(0, len(c), 2)]
 
 
-def plot_bins_from_geojson(geojson_file, output_dir,part = 0): #part - dio detektora, funkcija prima 0 za E ili 1 za H 
-        # Read GeoJSON file
-        with open(geojson_file, 'r') as f:
-            data = json.load(f)
-        
-        # Dictionary to store bins grouped by layer
-        layer_bins = {}
-        
-        # Iterate over each feature in the GeoJSON file
-        for feature in data['features']:
-            layer = feature['properties']['Layer']
-            geometry = shape(feature['geometry'])  # Convert GeoJSON geometry to Shapely geometry
-            
-            # Add the geometry to the corresponding layer
-            if layer not in layer_bins:
-                layer_bins[layer] = [geometry]
+def plot_bins_from_geojson(geojson_file, output_dir, part=0):  # part - part of the detector, function takes 0 for E or 1 for H
+    # Read GeoJSON file
+    with open(geojson_file, 'r') as f:
+        data = json.load(f)
+
+    # Dictionary to store bins grouped by layer
+    layer_bins = {}
+
+    # Iterate over each feature in the GeoJSON file
+    for feature in data['features']:
+        layer = feature['properties']['Layer']
+        geometry = shape(feature['geometry'])  # Convert GeoJSON geometry to Shapely geometry
+
+        # Add the geometry to the corresponding layer
+        if layer not in layer_bins:
+            layer_bins[layer] = [geometry]
+        else:
+            layer_bins[layer].append(geometry)
+    choosen_layer = int(input("Choose ploting layer(1-50): "))
+    bins = layer_bins.get(choosen_layer, [])  # draw for the 50th layer
+
+    # Plot bins
+    if bins:
+        cmap = plt.cm.viridis
+        norm = plt.Normalize(vmin=0, vmax=max(coords[part][1]))
+        colorbar = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
+        colorbar.set_array([])
+        fig = plt.figure(figsize=(8, 6))
+        eta = 19  # starting coordinate for drawing
+        phi = 53
+        for idx, bin_geometry in enumerate(bins):
+            if [eta, phi] in coords[part][0]:
+                if coords[part][0].count([eta, phi]) > 1:
+                    print(f"Overlap at coordinates {eta, phi}")
+                index = coords[part][0].index([eta, phi])
+                plt.fill(*bin_geometry.exterior.xy, color=colorbar.to_rgba(coords[part][1][index]))
             else:
-                layer_bins[layer].append(geometry)
-        
-        bins = layer_bins.get(50, []) #crtamo za 50ti layer
-        
-        # Plot bins
-        if bins:
-                cmap = plt.cm.viridis
-                norm = plt.Normalize(vmin=0, vmax=max(coord[part][1]))
-                colorbar = plt.cm.ScalarMappable(norm=norm, cmap=cmap)
-                colorbar.set_array([])
-                fig = plt.figure(figsize=(8, 6))
-                eta = 19 #početna koordinata crtanja
-                phi = 53 
-                for idx, bin_geometry in enumerate(bins):
-                    if [eta,phi] in coord[part][0]:
-                        if coord[part][0].count([eta,phi]) > 1:
-                            print(f"Preklapanje na koordinati {eta,phi}")
-                        indeks = coord[part][0].index([eta,phi])
-                        plt.fill(*bin_geometry.exterior.xy, color=colorbar.to_rgba(coord[part][1][indeks]))
-                    else:
-                        plt.plot(*bin_geometry.exterior.xy, color='black', linewidth=0.5)
-                    plt.gca().set_aspect('equal', adjustable='datalim')
+                plt.plot(*bin_geometry.exterior.xy, color='black', linewidth=0.5)
+            plt.gca().set_aspect('equal', adjustable='datalim')
 
-                    #jednostavni algoritam koji prati koordinatu koju se trenutno crta
-                    eta -= 1
-                    if eta < 0:
-                        phi -= 1
-                        eta = 19
-                        if phi < 0:
-                            phi = 71
+            # simple algorithm that follows the currently drawn coordinate
+            eta -= 1
+            if eta < 0:
+                phi -= 1
+                eta = 19
+                if phi < 0:
+                    phi = 71
 
-                for i in range(10, 251, 10):  # Start from 10 cm to 250 cm, increment by 10
-                    plt.plot([-1, 1], [i, i], color='black', linewidth=0.5) 
-                if part == 0:
-                    plt.title(f'Suma outputa 14 Stage-1 FPG --- E data')
-                else:
-                    plt.title(f'Suma outputa 14 Stage-1 FPG --- H data')
-                plt.xlabel('X Position')
-                plt.ylabel('Y Position')
-                plt.grid(True)
-                
-                # Save the plot as a PNG file
-                output_file = os.path.join(output_dir, f'sum_output_stage1_{part}.png')
-                print(output_file)
-                plt.savefig(output_file)
-                plt.close() 
-                add_colorbar("out",max(coord[part][1])) #dodajemo colorbar na sve slike koje se nalaze u mapi out
+        for i in range(10, 251, 10):  # Start from 10 cm to 250 cm, increment by 10
+            plt.plot([-1, 1], [i, i], color='black', linewidth=0.5)
+        if part == 0:
+            plt.title(f'Sum of output 14 Stage-1 FPG --- E data')
+        else:
+            plt.title(f'Sum of output 14 Stage-1 FPG --- H data')
+        plt.xlabel('X Position')
+        plt.ylabel('Y Position')
+        plt.grid(True)
 
-def add_colorbar(path,color_max):
-    # Iteracija kroz slike u mapi A
+        # Save the plot as a PNG file
+        output_file = os.path.join(output_dir, f'sum_output_stage1_{part}.png')
+        print(output_file)
+        plt.savefig(output_file)
+        plt.close()
+        add_colorbar("out", max(coords[part][1]))  # add a colorbar to all images in the "out" folder
+
+
+def add_colorbar(path, color_max):
+    # Iteration through images in folder "out"
     for filename in os.listdir(path):
-        if filename.endswith(".png") and "_colorbar" not in filename: #Provjera ekstenzije slike te da vec nema colorbar
-            # Učitavanje slike iz mape A
+        if filename.endswith(".png") and "_colorbar" not in filename:  # Check the image extension and if it does not already have a colorbar
+            # Load image from folder "out"
             img = plt.imread(os.path.join(path, filename))
             fig, ax = plt.subplots(figsize=(15, 10))
             ax.imshow(img)
-            ax.axis('off')  # Isključivanje oznaka osi
-            # Stvaranje colorbara na desnoj strani
+            ax.axis('off')  # Turn off axis labels
+            # Create colorbar on the right side
             divider = make_axes_locatable(ax)
-            cax = divider.append_axes("right", size="5%", pad=0.04 ) 
-            norm = Normalize(vmin=0, vmax=color_max) #raspon colorbar
-            
-            cbar = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap='viridis'), cax=cax,fraction=0.046, aspect=1)
-            cbar.set_label('Vrijednosti sume')
-            filename = filename.replace(".png", "")
-            plt.savefig(os.path.join(path, filename + '_colorbar.png'), bbox_inches='tight')
-        
-            plt.close() 
-            
-plot_bins_from_geojson("towers_bins_only_vertices.geojson","out",0)
-plot_bins_from_geojson("towers_bins_only_vertices.geojson","out",1)
+            cax = divider.append_axes("right", size="5%", pad=0.04)
+            norm = Normalize(vmin=0, vmax=color_max)  # colorbar range
+
+            cbar = plt.colorbar(plt.cm.ScalarMappable(norm=norm, cmap='viridis'), cax=cax, fraction=0.046, aspect=1)
+            cbar.set_label('Sum Values')
+            filename_new = filename.replace(".png", "")
+            plt.savefig(os.path.join(path, filename_new + '_colorbar.png'), bbox_inches='tight')
+
+            plt.close()
+            os.remove(os.path.join(path,filename))
+
+plot_bins_from_geojson("towers_bins_only_vertices.geojson", "out", 0)
+plot_bins_from_geojson("towers_bins_only_vertices.geojson", "out", 1)
--- Plot/sum_outputs.py
+++ Plot/sum_outputs.py
@@ -4,51 +4,51 @@ import os
 
 os.chdir('../outputs')
 files = [[], []]
-for file_name in os.listdir():  #učitavamo imena svih datoteke iz mape 'outputs' u dva zasebna niza
+for file_name in os.listdir():  # load names of all files from the 'outputs' folder into two separate arrays
     if file_name.startswith("CE_E"):
         files[0].append(file_name)
     elif file_name.startswith("CE_H"):
         files[1].append(file_name)
 
 for i in range(2):
-    line_count = 0 #broj linija potrebnih za sumu, jednak broju ulaznih koordinata
-    with open(files[i][0], 'r') as file: 
+    line_count = 0  # number of lines needed for sum, equal to the number of input coordinates
+    with open(files[i][0], 'r') as file:
         for line in file:
             line_count += 1
-    sum = np.zeros(line_count)
+    sum_values = np.zeros(line_count)
 
-    for file in files[i]: 
+    for file in files[i]:
         j = 0
         with open(file, 'r') as f:
             lines = f.readlines()
             for line in lines:
-                sum[j] += int(line.strip(", \n")) 
-                j += 1    
-    
-    if i == 0: #ime filea ovisno koje podatke gledamo
+                sum_values[j] += int(line.strip(", \n"))
+                j += 1
+
+    if i == 0:  # filename depending on the data we are looking at
         name = 'out_sum_E.txt'
     elif i == 1:
         name = 'out_sum_H.txt'
-    with open('../Plot/' + name, 'w') as f: #zapisujemo sume za svaku koord
-        for j in range (len(sum)):
-            f.write(str(sum[j]) + '\n')
+    with open('../Plot/' + name, 'w') as f:  # write sums for each coordinate
+        for j in range(len(sum_values)):
+            f.write(str(sum_values[j]) + '\n')
 
-    #dio koda za citanje koordinata        
+    # code part for reading coordinates
     eta_coordinates = []
     phi_coordinates = []
-    
-    if i == 0: #dva input file iz kojih cemo isčitati koordinate
+
+    if i == 0:  # two input files from which we will extract coordinates
         name = 'coord_E.txt'
     elif i == 1:
         name = 'coord_H.txt'
     with open('../Plot/' + name, 'r') as file:
         lines = file.readlines()
         for line in lines:
-            # Pronalazak koordinata eta i fi u liniji pomoću regularnog izraza
+            # Finding eta and phi coordinates in the line using regular expression
             if i == 0:
-                match = re.match(r'/\* out\d+_em-eta(-?\d+)-phi(-?\d+) \*/\s+(.*)', line)
+                match = re.match(r'/\* out\d+_em-eta(-?\d+)-phi(-?\d+) \*/\s+(.*)', line)  # change if input files change
             else:
-                match = re.match(r'/\* out\d+_had-eta(-?\d+)-phi(-?\d+) \*/\s+(.*)', line)
+                match = re.match(r'/\* out\d+_had-eta(-?\d+)-phi(-?\d+) \*/\s+(.*)', line)  # All input files have the same coordinates at the same positions
             if match:
                 eta = int(match.group(1))
                 phi = int(match.group(2))
@@ -56,20 +56,19 @@ for i in range(2):
                 phi_coordinates.append(phi)
             else:
                 print(line)
-                print("Dogodila se greška, sve koordinate nisu ucitane - prekidam program")
-                exit()                           
-                
-    if i == 0: #ime filea ovisno koje podatke gledamo
+                print("Error occurred, not all coordinates were loaded - exiting the program")
+                exit()
+
+    if i == 0:  # filename depending on the data we are looking at
         name_eta = 'out_eta_E.txt'
         name_phi = 'out_phi_E.txt'
     elif i == 1:
         name_eta = 'out_eta_H.txt'
         name_phi = 'out_phi_H.txt'
     with open('../Plot/' + name_phi, 'w') as f:
-        for i in range (len(phi_coordinates)):
+        for i in range(len(phi_coordinates)):
             f.write(str(phi_coordinates[i]) + '\n')
-            
+
     with open('../Plot/' + name_eta, 'w') as f:
-        for i in range (len(eta_coordinates)):
+        for i in range(len(eta_coordinates)):
             f.write(str(eta_coordinates[i]) + '\n')
-            
